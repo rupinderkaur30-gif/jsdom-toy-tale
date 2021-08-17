1a761eb9dbd5daff04782feaa05c56608451eb76
rupinderkaur <rupi063094@gmail.com>
2021-08-16 20:00:11 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
+const addBtn = document.querySelector("#new-toy-btn");
+  const toyFormContainer = document.querySelector(".container");
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
-  const addBtn = document.querySelector("#new-toy-btn");
-  const toyFormContainer = document.querySelector(".container");
+    fetch("http://localhost:3000/toys")
+    .then(response => response.json())
+    .then(toys => {
+    let toysHTML = toys.map(function(toy){
+      return `  
+      <div class="card">
+      <h2>${toy.name}</h2>
+      <img src=${toy.image}class="toy-avatar" />
+      <p>${toy.likes} Likes </p>
+      <button class="like-btn">Like <3</button>
+    </div>
+    `
+    })
+    document.querySelector("#toy-collection").innerHTML = toysHTML.join('')
+  
+  toyFormContainer.addEventListener("submit", function(e){
+    e.preventDefault
+    console.log(e.target)
+  })
+})
+ 
+ 
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
@@ -13,3 +35,4 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
